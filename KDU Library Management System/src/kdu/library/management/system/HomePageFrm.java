662bc0c8da5c0b38358635e21c16c95e48176b1c
nadinCodeHat <nadinpethiyagoda4@gmail.com>
2021-08-25 16:16:56 +0530

    Login for admin and librarian

--- KDU Library Management System/src/kdu/library/management/system/HomePageFrm.java
+++ KDU Library Management System/src/kdu/library/management/system/HomePageFrm.java
@@ -25,8 +25,7 @@ public class HomePageFrm extends javax.swing.JFrame {
         copyright = new javax.swing.JLabel();
         logo = new javax.swing.JLabel();
         enterBtn = new javax.swing.JButton();
-        adminBtn = new javax.swing.JButton();
-        librarianBtn = new javax.swing.JButton();
+        loginBtn = new javax.swing.JButton();
         msg = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -37,20 +36,16 @@ public class HomePageFrm extends javax.swing.JFrame {
         jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         copyright.setFont(new java.awt.Font("Segoe UI", 0, 12)); // NOI18N
-        copyright.setForeground(new java.awt.Color(112, 112, 112));
         copyright.setText("© KDU 2020");
-        jPanel1.add(copyright, new org.netbeans.lib.awtextra.AbsoluteConstraints(400, 330, -1, -1));
+        jPanel1.add(copyright, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 330, -1, -1));
 
-        logo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/resources/kdu_logo_small.png"))); // NOI18N
+        logo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/resources/kdu_logo.png"))); // NOI18N
         jPanel1.add(logo, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 50, 100, 80));
 
         enterBtn.setIcon(new javax.swing.ImageIcon(getClass().getResource("/resources/enterBtn.png"))); // NOI18N
         enterBtn.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(112, 112, 112)));
         enterBtn.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         enterBtn.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                enterBtnMouseClicked(evt);
-            }
             public void mouseEntered(java.awt.event.MouseEvent evt) {
                 enterBtnMouseEntered(evt);
             }
@@ -58,44 +53,32 @@ public class HomePageFrm extends javax.swing.JFrame {
                 enterBtnMouseExited(evt);
             }
         });
-        jPanel1.add(enterBtn, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 200, 90, 90));
-
-        adminBtn.setIcon(new javax.swing.ImageIcon(getClass().getResource("/resources/adminBtn.png"))); // NOI18N
-        adminBtn.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(112, 112, 112)));
-        adminBtn.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-        adminBtn.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseEntered(java.awt.event.MouseEvent evt) {
-                adminBtnMouseEntered(evt);
-            }
-            public void mouseExited(java.awt.event.MouseEvent evt) {
-                adminBtnMouseExited(evt);
-            }
-        });
-        adminBtn.addActionListener(new java.awt.event.ActionListener() {
+        enterBtn.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                adminBtnActionPerformed(evt);
+                enterBtnActionPerformed(evt);
             }
         });
-        jPanel1.add(adminBtn, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 200, 90, 90));
+        jPanel1.add(enterBtn, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 190, 90, 90));
 
-        librarianBtn.setIcon(new javax.swing.ImageIcon(getClass().getResource("/resources/librarianBtn.png"))); // NOI18N
-        librarianBtn.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(112, 112, 112)));
-        librarianBtn.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-        librarianBtn.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                librarianBtnMouseClicked(evt);
-            }
+        loginBtn.setIcon(new javax.swing.ImageIcon(getClass().getResource("/resources/loginBtn.png"))); // NOI18N
+        loginBtn.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(112, 112, 112)));
+        loginBtn.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        loginBtn.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseEntered(java.awt.event.MouseEvent evt) {
-                librarianBtnMouseEntered(evt);
+                loginBtnMouseEntered(evt);
             }
             public void mouseExited(java.awt.event.MouseEvent evt) {
-                librarianBtnMouseExited(evt);
+                loginBtnMouseExited(evt);
             }
         });
-        jPanel1.add(librarianBtn, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 200, 90, 90));
+        loginBtn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                loginBtnActionPerformed(evt);
+            }
+        });
+        jPanel1.add(loginBtn, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 190, 90, 90));
 
         msg.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
-        msg.setForeground(new java.awt.Color(112, 112, 112));
         msg.setText("Welcome to KDU Library Management System");
         jPanel1.add(msg, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 140, -1, -1));
 
@@ -105,49 +88,23 @@ public class HomePageFrm extends javax.swing.JFrame {
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
-    private void adminBtnMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_adminBtnMouseEntered
+    private void loginBtnMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_loginBtnMouseEntered
         try {
-            Image adminBtnHover = ImageIO.read(getClass().getResource("/resources/adminBtnHover.png"));
-            adminBtn.setIcon(new ImageIcon(adminBtnHover));
+            Image librarianBtnHover = ImageIO.read(getClass().getResource("/resources/loginBtnHover.png"));
+            loginBtn.setIcon(new ImageIcon(librarianBtnHover));
         } catch (IOException ex) {
             Logger.getLogger(HomePageFrm.class.getName()).log(Level.SEVERE, null, ex);
         }
-    }//GEN-LAST:event_adminBtnMouseEntered
+    }//GEN-LAST:event_loginBtnMouseEntered
 
-    private void adminBtnMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_adminBtnMouseExited
+    private void loginBtnMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_loginBtnMouseExited
         try {
-            Image adminBtnImg = ImageIO.read(getClass().getResource("/resources/adminBtn.png"));
-            adminBtn.setIcon(new ImageIcon(adminBtnImg));
+            Image librarianBtnImg = ImageIO.read(getClass().getResource("/resources/loginBtn.png"));
+            loginBtn.setIcon(new ImageIcon(librarianBtnImg));
         } catch (IOException ex) {
             Logger.getLogger(HomePageFrm.class.getName()).log(Level.SEVERE, null, ex);
         }
-    }//GEN-LAST:event_adminBtnMouseExited
-
-    private void adminBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_adminBtnActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_adminBtnActionPerformed
-
-    private void librarianBtnMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_librarianBtnMouseEntered
-        try {
-            Image librarianBtnHover = ImageIO.read(getClass().getResource("/resources/librarianBtnHover.png"));
-            librarianBtn.setIcon(new ImageIcon(librarianBtnHover));
-        } catch (IOException ex) {
-            Logger.getLogger(HomePageFrm.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }//GEN-LAST:event_librarianBtnMouseEntered
-
-    private void librarianBtnMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_librarianBtnMouseExited
-        try {
-            Image librarianBtnImg = ImageIO.read(getClass().getResource("/resources/librarianBtn.png"));
-            librarianBtn.setIcon(new ImageIcon(librarianBtnImg));
-        } catch (IOException ex) {
-            Logger.getLogger(HomePageFrm.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }//GEN-LAST:event_librarianBtnMouseExited
-
-    private void librarianBtnMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_librarianBtnMouseClicked
-        // TODO add your handling code here:
-    }//GEN-LAST:event_librarianBtnMouseClicked
+    }//GEN-LAST:event_loginBtnMouseExited
 
     private void enterBtnMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_enterBtnMouseEntered
         try {
@@ -167,9 +124,14 @@ public class HomePageFrm extends javax.swing.JFrame {
         }
     }//GEN-LAST:event_enterBtnMouseExited
 
-    private void enterBtnMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_enterBtnMouseClicked
-        // TODO add your handling code here:
-    }//GEN-LAST:event_enterBtnMouseClicked
+    private void loginBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_loginBtnActionPerformed
+        LoginFrm lgnFrm = new LoginFrm();
+        lgnFrm.setVisible(true);
+    }//GEN-LAST:event_loginBtnActionPerformed
+
+    private void enterBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_enterBtnActionPerformed
+        
+    }//GEN-LAST:event_enterBtnActionPerformed
 
     /**
      * @param args the command line arguments
@@ -203,11 +165,10 @@ public class HomePageFrm extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton adminBtn;
     private javax.swing.JLabel copyright;
     private javax.swing.JButton enterBtn;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JButton librarianBtn;
+    private javax.swing.JButton loginBtn;
     private javax.swing.JLabel logo;
     private javax.swing.JLabel msg;
     // End of variables declaration//GEN-END:variables
---
+++ KDU Library Management System/src/kdu/library/management/system/LoginFrm.java
@@ -0,0 +1,186 @@
+package kdu.library.management.system;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author nadinCodeHat
+ */
+public class LoginFrm extends javax.swing.JFrame {
+
+    public LoginFrm(){
+        initComponents();
+    }
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jPanel1 = new javax.swing.JPanel();
+        logo = new javax.swing.JLabel();
+        loginPanel = new javax.swing.JPanel();
+        emailLabel = new javax.swing.JLabel();
+        passwordLabel = new javax.swing.JLabel();
+        passwordTextField = new javax.swing.JPasswordField();
+        emailTextField = new javax.swing.JTextField();
+        exitBtn = new javax.swing.JButton();
+        loginBtn = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        setResizable(false);
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        logo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/resources/kdu_logo_small.png"))); // NOI18N
+        jPanel1.add(logo, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 10, 60, 48));
+
+        loginPanel.setBackground(new java.awt.Color(255, 255, 255));
+        loginPanel.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Login", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Segoe UI", 0, 12))); // NOI18N
+        loginPanel.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        emailLabel.setFont(new java.awt.Font("Segoe UI", 0, 12)); // NOI18N
+        emailLabel.setText("Email");
+        loginPanel.add(emailLabel, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 40, -1, -1));
+
+        passwordLabel.setFont(new java.awt.Font("Segoe UI", 0, 12)); // NOI18N
+        passwordLabel.setText("Password");
+        loginPanel.add(passwordLabel, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 80, -1, -1));
+
+        passwordTextField.setFont(new java.awt.Font("Segoe UI", 0, 12)); // NOI18N
+        loginPanel.add(passwordTextField, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 80, 170, -1));
+
+        emailTextField.setFont(new java.awt.Font("Segoe UI", 0, 12)); // NOI18N
+        loginPanel.add(emailTextField, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 40, 170, -1));
+
+        exitBtn.setText("Exit");
+        exitBtn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                exitBtnActionPerformed(evt);
+            }
+        });
+        loginPanel.add(exitBtn, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 120, 60, -1));
+
+        loginBtn.setText("Login");
+        loginBtn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                loginBtnActionPerformed(evt);
+            }
+        });
+        loginPanel.add(loginBtn, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 120, 60, -1));
+
+        jPanel1.add(loginPanel, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 60, 300, 170));
+
+        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 320, 240));
+
+        pack();
+        setLocationRelativeTo(null);
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void exitBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_exitBtnActionPerformed
+        this.dispose();
+    }//GEN-LAST:event_exitBtnActionPerformed
+
+    private void loginBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_loginBtnActionPerformed
+        if (validateFields()) {
+            String email = emailTextField.getText();
+            String password = String.valueOf(passwordTextField.getPassword());
+
+            String query = "SELECT users.name, users.email, users.password, roles.role_name FROM `users` INNER JOIN `roles` ON roles.id = users.role_id AND users.email=? AND users.password=?";
+            try {
+                ResultSet rs;
+                try (PreparedStatement pst = DBConnectClass.getConnection().prepareStatement(query)) {
+                    pst.setString(1, email);
+                    pst.setString(2, password);
+                    rs = pst.executeQuery();
+                    if (rs.next()) {
+                        String name = rs.getString("name");
+                        if (rs.getString("email").equals(email) && rs.getString("role_name").equals("Admin")) {               
+                            JOptionPane.showMessageDialog(null, "Login Successful! Welcome "+name);
+                            AdminFrm adminFrm = new AdminFrm();
+                            adminFrm.setVisible(true);
+                            this.dispose();
+                        } else {
+                            JOptionPane.showMessageDialog(null, "Login Successful! Welcome "+name);
+                            LibrarianFrm libFrm = new LibrarianFrm();
+                            libFrm.setVisible(true);
+                            this.dispose();
+                        }
+                    } else {
+                        JOptionPane.showMessageDialog(null, "Invalid Email or Password", "Access Denied", JOptionPane.ERROR_MESSAGE);
+                    }
+                }
+                rs.close();
+                DBConnectClass.getConnection().close();
+            } catch (SQLException ex) {
+                Logger.getLogger(LoginFrm.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }//GEN-LAST:event_loginBtnActionPerformed
+
+     //validate fields
+    public boolean validateFields() {
+        String email = emailTextField.getText();
+        String password = String.valueOf(passwordTextField.getPassword());
+
+        //check empty fields
+        if (email.trim().equals("")){
+            JOptionPane.showMessageDialog(null, "Please enter your email address.", "Empty Field", 2);
+            return false;
+        }
+        if(password.trim().equals("")){
+            JOptionPane.showMessageDialog(null, "Password cannot be empty!", "Empty Field", 2);
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Windows".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(LoginFrm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+        
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(() -> {
+            new LoginFrm().setVisible(true);
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel emailLabel;
+    private javax.swing.JTextField emailTextField;
+    private javax.swing.JButton exitBtn;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JButton loginBtn;
+    private javax.swing.JPanel loginPanel;
+    private javax.swing.JLabel logo;
+    private javax.swing.JLabel passwordLabel;
+    private javax.swing.JPasswordField passwordTextField;
+    // End of variables declaration//GEN-END:variables
+}
